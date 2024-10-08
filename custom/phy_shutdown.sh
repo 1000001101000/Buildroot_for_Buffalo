cf8c31efee1598d5e0e36fd68d1321e01627725e
Jeremy J. Peper <1000001101000>
2024-10-08 15:44:05 -0500

    start making script more dynamic

--- custom/phy_shutdown.sh
+++ custom/phy_shutdown.sh
@@ -2,22 +2,28 @@
 # place in /lib/systemd/system-shutdown/ for debian based systems
 # other systems it's /usr/lib/systemd/system-shutdown/
 
-phytool="/usr/local/bin/phytool"
+#phytool="/usr/local/bin/phytool"
 
 ##I think only needed for dhcp leases file
+##overlay ramfs or just link up?
 mount -o remount,rw /
 
 ifup --no-scripts --force eth0
 sleep 2
 mount -o remount,ro /
-
-$phytool write eth0/0/22 3
+##can we get from devtree?
+phytool write eth0/0/22 3
+before="$(phytool read eth0/0/16)"
 if [ "$1" == "halt" ] || [ "$1" == "poweroff" ]; then
-    $phytool write eth0/0/16 0x0881
+  new="0x0881"
 else
-    $phytool write eth0/0/16 0x0981
+  new="0x0981"
 fi
-$phytool write eth0/0/22 0
+echo "phy_shutdown, try to set $new"
+phytool write eth0/0/16 $new
+after="$(phytool read eth0/0/16)"
+phytool write eth0/0/22 0
+echo "phy_shutdown: $before -> $after"
 
 exit 0
 
