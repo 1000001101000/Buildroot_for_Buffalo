35789d8a333f3666d0be510ac44dc35bfd61c062
Jeremy J. Peper <1000001101000>
2025-01-22 14:38:14 -0600

    pull in needed binaries and scripts but not the non-functional initscripts etc

--- scripts/alpine_finish.sh
+++ scripts/alpine_finish.sh
@@ -11,7 +11,7 @@ debian_import libreadline5 buster armhf main "./lib/arm-linux-gnueabihf/"
 debian_import libtinfo6 buster armhf main "./lib/arm-linux-gnueabihf/"
 
 ##grab older mdadm binary compatible with this kernel's interfaces
-debian_import mdadm jessie armhf main "./etc/ ./lib/udev/ ./sbin/ ./usr/share/mdadm/"
+debian_import mdadm jessie armhf main "./etc/cron.daily/ ./etc/cron.d/ ./etc/logcheck/ ./lib/udev/ ./sbin/ ./usr/share/mdadm/"
 sed -i 's/ root / /g' "$TARGET_DIR/etc/cron.d/mdadm"
 
 ##tweak to handle cases where usb nic appears later than expected
