7efa02b2947b7b1458f1cf6262b92ec6538bf40d
Jeremy J. Peper <1000001101000>
2025-03-10 15:46:39 -0500

    updates to clean up and consolidate

--- scripts/ls700_finish.sh
+++ scripts/ls700_finish.sh
@@ -4,13 +4,4 @@
 
 ../scripts/common_finish
 
-##grab xfs programs compatabile with the older xfs version supported by kernel
-##also grab readline5 library needed by some of them
-debian_import xfsprogs stretch arm64 main "./usr/sbin/ ./sbin/"
-debian_import libreadline5 buster arm64 main "./lib/aarch64-linux-gnu/"
-debian_import libtinfo6 buster arm64 main "./lib/aarch64-linux-gnu/"
-
-cgroupv1_tweak
-
-cp "$custom_dir"/S79r8152 "$TARGET_DIR/etc/init.d/"
 exit 0
--- scripts/ls700_postbuild.sh
+++ scripts/ls700_postbuild.sh
@@ -9,21 +9,12 @@ rootfs_type="ext4"
 ##pad the dtbs so uboot can update them
 pad_dtbs
 
-generate_initrd "ls700" "$rootfsID" "$bootID"
+generate_initrd_uboot
 
-###we sure we can't generate this in the kernel build?
-#gzip -f -n -9 -k "$BINARIES_DIR/Image"
-#mkimage -A arm64 -O linux -T kernel -C none -a 0X2200000 -e 0X2200000 -n buildroot-kernel-$datesuff -d "$BINARIES_DIR/Image" "$BINARIES_DIR/uImage-generic.buffalo"
+gen_appended_uImage
 
-gzip -f -k "$BINARIES_DIR/rootfs.ext2"
-
-bootfs_copy "$BINARIES_DIR/initrd.gz"
-bootfs_copy "$BINARIES_DIR/rootfs.ext2.gz"
-bootfs_copy "$BINARIES_DIR/rtd1619-ls710.dtb"
-bootfs_copy "$BINARIES_DIR/rtd1619-ls720.dtb"
-bootfs_copy "$BINARIES_DIR/Image"
-#bootfs_copy "$BINARIES_DIR/uImage-generic.buffalo"
-
+bootfs_copy "$BINARIES_DIR/rtd-1619-mmnas-megingjord-2GB.dtb" "rtd1619-ls700.dtb"
+bootfs_copy "../scripts/ls700_install.sh"
 create_image
 
 exit 0
